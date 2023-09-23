c315e090b22f591b39844dc6adf499052e7a72d8
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2023-09-24 00:23:30 +0700

    ui-map in place

--- src/places/components/placeItemComponent/PlaceItem.jsx
+++ src/places/components/placeItemComponent/PlaceItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Card, Modal } from '../../../shared'
+import { Button, Card, Modal, Map } from '../../../shared'
 import './PlaceItem.scss'
 const PlaceItem = (props) => {
   const { key, id, title, image, description, address, creatorId, coordinates } = props
@@ -24,7 +24,15 @@ const PlaceItem = (props) => {
         footerClass='place-item__modal-actions'
       >
         <div className='map-container'>
-          <h2>THE MAP!</h2>
+
+          <Map address={address} />
+          {/* <iframe title="map" width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"
+            src={'https://maps.google.com/maps?q=' + props.coordinates.lat.toString() + ',' + props.coordinates.lng.toString() + '&t=&z=15&ie=UTF8&iwloc=&output=embed'}>
+          </iframe>
+          <script
+            type='text/javascript'
+            src='https://embedmaps.com/google-maps-authorization/script.js?id=5a33be79e53caf0a07dfec499abf84b7b481f165'>
+          </script> */}
         </div>
       </Modal>
       <li className='place-item'>
--- src/shared/index.js
+++ src/shared/index.js
@@ -5,5 +5,6 @@ export { default as Avatar } from './components/UIElements/Avatar'
 export { default as MainNavigation } from './components/Navaigation/navigationComponent/MainNavaigation'
 export { default as Button } from './components/FormElements/Button'
 export { default as Modal } from './components/UIElements/modalComponent/Modal'
+export { default as Map } from './components/UIElements/map/Map'
 
 
